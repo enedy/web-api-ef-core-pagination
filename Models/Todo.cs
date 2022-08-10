05b9b283a237e83f81745158d1337b43dee38797
Enedy <enedy.cordeiro@certponto.com.br>
2022-08-09 22:57:31 -0300

    initial commit

---
+++ Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiEfCorePagination.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
